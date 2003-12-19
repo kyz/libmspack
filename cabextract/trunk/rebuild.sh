7d417d6f5e0fe9d23e150c1101ab23fbf57b4d06
kyz <kyz@18a31f5d-9092-44d1-ac33-008f6764bcc6>
2003-12-19 14:23:48 +0000

    Better cvs-build scripts

--- cabextract/trunk/rebuild.sh
+++ cabextract/trunk/rebuild.sh
@@ -1,19 +1,7 @@
 #!/bin/sh
-# "./rebuild.sh" deletes all auto-generated files
-# "./rebuild.sh yes" makes a full clean, configure, build, dist and distcheck
-
-chmod -R +w cabextract-0.* 2>/dev/null || true
-
-rm -rf *.o aclocal.* autom4te.cache cabextract-0.* cabextract.spec config.* \
-configure COPYING INSTALL install-sh libmspack.a Makefile Makefile.in missing \
-mkinstalldirs mspack cabextract src/cabinfo stamp-h1 stamp-h1.in
-
-if [ "x$1" == "xyes" ]
-then
-  # obtain latest libmspack
-  sh import.sh
-  autoreconf -i -W all
-  ./configure
-  make
-  make distcheck
-fi
+./cleanup.sh
+./import.sh
+autoreconf -i -W all
+./configure
+make
+make distcheck
