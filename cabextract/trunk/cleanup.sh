2b0ddb34002953447e695534009aed1b2cadfa95
Stuart Caie <kyzer@4u.net>
2009-06-01 19:53:01 +0000

    change cleanup.sh to use svn:ignore rather than .cvsignore remove import.sh in favour of soft links

--- cabextract/trunk/cleanup.sh
+++ cabextract/trunk/cleanup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-
-chmod -R +w cabextract-1.* 2>/dev/null || true
-
-rm -rf *.o aclocal.* autom4te.cache cabextract-1.* cabextract.spec config.* \
-configure COPYING INSTALL install-sh libmspack.a Makefile Makefile.in missing \
-mkinstalldirs mspack cabextract src/cabinfo stamp-h1 stamp-h1.in
+topdir=`pwd`
+for x in . src
+do
+  cd $x
+  chmod -R a+rwx `svn pg svn:ignore .` 2>/dev/null
+  rm -vrf `svn pg svn:ignore .`
+  cd $topdir
+done
---
+++ cabextract/trunk/mspack/cabd.c
@@ -0,0 +1,1 @@
+../../../libmspack/trunk/mspack/cabd.c
