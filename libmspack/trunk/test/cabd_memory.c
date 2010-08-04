219e12891b264245285f3cecb06ed99ea15c53ef
kyz <kyz@18a31f5d-9092-44d1-ac33-008f6764bcc6>
2010-08-04 13:27:31 +0000

    Remove unnecessary inclusion of <unistd.h>

--- libmspack/trunk/test/cabd_memory.c
+++ libmspack/trunk/test/cabd_memory.c
@@ -7,7 +7,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <mspack.h>
 
 /* use a pointer to a mem_buf structure as "filenames" */
--- libmspack/trunk/test/expand.c
+++ libmspack/trunk/test/expand.c
@@ -6,7 +6,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 #include <mspack.h>
 #include <error.h>
