f0d26db1fa5dcfe0f562a34a70d0cb35f100d1d0
Stuart Caie <kyzer@4u.net>
2015-05-10 11:50:12 +0000

    Add automake subdir-objects option and AM_PROG_AR as suggested by autoreconf

--- libmspack/trunk/cleanup.sh
+++ libmspack/trunk/cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 topdir=`pwd`
-for x in . doc test
+for x in . doc mspack test
 do
   cd $x
   chmod -R a+rwx `svn pg svn:ignore .` 2>/dev/null
--- libmspack/trunk/rebuild.sh
+++ libmspack/trunk/rebuild.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-./cleanup.sh
-mkdir m4
-autoreconf -i -W all
-./configure
-make
-make -C doc
-make distcheck
+./cleanup.sh && mkdir m4 && autoreconf -i -W all &&
+./configure && make && make -C doc && make distcheck
