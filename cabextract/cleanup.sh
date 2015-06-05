cef3a03b74cc49a804af1fbf26aa073d600c8eb4
Stuart Caie <kyzer@4u.net>
2015-06-05 09:37:34 +0100

    Move svn:ignore properties into .gitignore files

--- cabextract/cleanup.sh
+++ cabextract/cleanup.sh
@@ -3,7 +3,7 @@ topdir=`pwd`
 for x in . mspack src
 do
   cd $x
-  chmod -R a+rwx `svn pg svn:ignore .` 2>/dev/null
-  rm -vrf `svn pg svn:ignore .`
+  chmod -R a+rwx `cat .gitignore` 2>/dev/null
+  rm -vrf `cat .gitignore`
   cd $topdir
 done
