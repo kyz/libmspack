66e850fc9ed8f11ecf4fc4cb958e0fddf3edf69f
Stuart Caie <kyzer@cabextract.org.uk>
2018-10-31 10:21:08 +0000

    Improve cleanup.sh

--- cabextract/cleanup.sh
+++ cabextract/cleanup.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 # deletes all auto-generated / compiled files
 
-topdir="`pwd`"
-for x in . mspack src test
-do
-  cd "$topdir/$x"
-  chmod -R a+rwx `cat .gitignore` 2>/dev/null
-  rm -vrf `cat .gitignore`
+for dir in . mspack src test; do
+    while read path; do
+        chmod -R a+rwx $dir/$path 2>/dev/null
+        rm -vrf $dir/$path
+    done < $dir/.gitignore
 done
