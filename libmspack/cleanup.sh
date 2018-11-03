9596b113fc9f2145ad05c2d7d75f340fcc2c078a
Stuart Caie <kyzer@cabextract.org.uk>
2018-11-03 19:29:35 +0000

    move the programs in src/ to examples/

--- libmspack/cleanup.sh
+++ libmspack/cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # deletes all auto-generated / compiled files
 
-for dir in . doc examples mspack src test; do
+for dir in . doc examples mspack test; do
     while read path; do
         chmod -R a+rwx $dir/$path 2>/dev/null
         rm -vrf $dir/$path
