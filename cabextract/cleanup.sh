4d747c2e385636732f15284af2b82973e47c36b6
Stuart Caie <kyzer@cabextract.org.uk>
2023-02-03 13:18:42 +0000

    cleanup.sh: use git clean -X

--- cabextract/cleanup.sh
+++ cabextract/cleanup.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 # deletes all auto-generated / compiled files
-
-for dir in . mspack src test; do
-    while read path; do
-        chmod -R a+rwx $dir/$path 2>/dev/null
-        rm -vrf $dir/$path
-    done < $dir/.gitignore
-done
+git clean -fX
